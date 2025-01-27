a05dedad414ea085af8b0438940b8b3d745f3bcf
jagtap-suraj <suraj552003@gmail.com>
2025-01-27 18:31:28 +0530

    Refactor database URI configuration for improved readability

--- config.py
+++ config.py
@@ -9,6 +9,9 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
 
     # AWS RDS MySQL Configuration
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USERNAME')}:{os.getenv(
-        'DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{os.getenv('DB_USERNAME')
+                           }:{os.getenv('DB_PASSWORD')}"
+        f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
